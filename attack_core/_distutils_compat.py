84dd392c1a4bb74c7e9e71b3059049605d15c4ec
poojakira <90021441+poojakira@users.noreply.github.com>
2026-08-15 21:32:28 -0700

    Make distutils shim dependency-free

--- attack_core/_distutils_compat.py
+++ attack_core/_distutils_compat.py
@@ -1,22 +1,44 @@
 from __future__ import annotations
 
+import re
 import sys
 import types
+from functools import total_ordering
 
-from packaging.version import Version
+
+@total_ordering
+class StrictVersion:
+    def __init__(self, vstring: object):
+        self.vstring = str(vstring)
+        self.version = tuple(int(part) for part in re.findall(r"\d+", self.vstring))
+
+    def __str__(self) -> str:
+        return self.vstring
+
+    def __repr__(self) -> str:
+        return f"StrictVersion({self.vstring!r})"
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, StrictVersion):
+            other = StrictVersion(other)
+        return self.version == other.version
+
+    def __lt__(self, other: object) -> bool:
+        if not isinstance(other, StrictVersion):
+            other = StrictVersion(other)
+        return self.version < other.version
 
 
 def ensure_distutils_version() -> None:
     try:
-        import distutils.version  # noqa: F401
-
+        __import__("distutils.version")
         return
     except ModuleNotFoundError:
         pass
 
     distutils_module = types.ModuleType("distutils")
     version_module = types.ModuleType("distutils.version")
-    version_module.StrictVersion = Version
-    distutils_module.version = version_module
+    setattr(version_module, "StrictVersion", StrictVersion)
+    setattr(distutils_module, "version", version_module)
     sys.modules.setdefault("distutils", distutils_module)
     sys.modules.setdefault("distutils.version", version_module)
